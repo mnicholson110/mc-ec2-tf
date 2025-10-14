6ac00f5d748bdb8487bb03c9db81ba0914be743e
Matt <mnicholson110@gmail.com>
2025-10-13 21:21:04 -0500

    cleanup outputs

--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,4 @@
-output "ami_id" {
-  description = "Baked AMI ID"
-  value       = module.image_builder.ami_id
-}
-
-output "instance_ids" {
-  description = "Managed EC2 instance IDs"
-  value       = module.ec2_instance.instance_ids
-}
-
-output "data_volume_id" {
-  description = "Persistent EBS data volume ID"
-  value       = try(module.ebs_data[0].volume_id, null)
-}
-
-output "eip_public_ip" {
-  description = "Elastic IP address (if allocated by Terraform)"
-  value       = try(aws_eip.static[0].public_ip, null)
-}
-
-output "launch_template_latest_version" {
-  description = "Launch template latest version"
-  value       = module.launch_template.launch_template_latest_version
+output "instance_is_running" {
+  description = "Is a server running?"
+  value       = var.instance_count == 1 ? "Yes" : "No"
 }
